1bd4952ca10b782549decd7db8a14d7f883b40db
GuessWh0o <developerint97@gmail.com>
2017-05-31 17:30:34 +0930

    Modifications

--- Intro Gates/Candies.cs
+++
@@ -1,7 +0,0 @@
-int candies(int n, int m) 
-{
-    int x, y;
-    x = m / n;
-    y = x * n;
-    return y;
-}
--- Intro Gates/LargestNumber.cs
+++
@@ -1,6 +0,0 @@
-int largestNumber(int n) 
-{
-    double a;
-    a = Math.Pow(10, n) - 1;
-    return (int)a;
-}
